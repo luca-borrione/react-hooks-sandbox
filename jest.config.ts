6dfaba53dcfa6ec7607e8f917ea00f92461c7591
Luca Borrione <23177229+luca-borrione@users.noreply.github.com>
2024-10-20 18:14:37 +0100

    re-config jest after react eject

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,48 @@
 const getConfig = async () => {
   return {
-    setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
+    collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+    moduleFileExtensions: [
+      "web.js",
+      "js",
+      "web.ts",
+      "ts",
+      "web.tsx",
+      "tsx",
+      "json",
+      "web.jsx",
+      "jsx",
+      "node",
+    ],
+    moduleNameMapper: {
+      "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+      "^react-native$": "react-native-web",
+    },
+    modulePaths: [],
+    resetMocks: true,
+    roots: ["<rootDir>/src"],
+    setupFiles: ["react-app-polyfill/jsdom"],
+    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+    testEnvironment: "jsdom",
+    testMatch: [
+      "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+      "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
+    ],
+    transform: {
+      "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$":
+        "<rootDir>/config/jest/babelTransform.js",
+      "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+      "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)":
+        "<rootDir>/config/jest/fileTransform.js",
+    },
+    transformIgnorePatterns: [
+      "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+      "^.+\\.module\\.(css|sass|scss)$",
+    ],
     verbose: true,
+    watchPlugins: [
+      "jest-watch-typeahead/filename",
+      "jest-watch-typeahead/testname",
+    ],
   };
 };
 
